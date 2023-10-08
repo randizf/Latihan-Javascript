eac7b4f945fb14609e77e032a296166475cfb88e
randizf <93034080+randizf@users.noreply.github.com>
2023-10-08 22:58:05 +0700

    latihan java script
    latihan

---
+++ nested if/main.java
@@ -0,0 +1,35 @@
+
+
+public class main {
+
+    public static void main(string[] args){
+
+        //nested if atau if bersarang
+
+        int a = 5;
+        int b = 10;
+
+        system.out.println("ini adalah awal dari program");
+
+        if (a == 5){
+
+            if (b == 10){
+
+                system.out.println("ini adalah dimana a = 5 dan b = 10");
+
+            } else {
+
+                system.out.println("ini adalah dimana a = 5 dan b bukan 10");
+                
+            }
+        
+        } else {
+
+            system.out.println("ini adalah dimana a dan b salah") 1;
+
+        }
+
+        system.out.println("ini adalah akhir dari program")
+
+    }
+}
---
+++ while loop/main.java
@@ -0,0 +1,26 @@
+public class Main {
+    
+    public static void main (string[] args); {
+
+
+//      while (kondisi) {
+//          aksi
+//      }
+
+        int a = 0;
+        boolean kondisi = true;
+
+        system.out.println("awal program");
+
+        while (true) {
+            system.out.println("while loop ke-" + a);
+
+            if (a == 10){
+
+            }
+        }
+
+        system.out.println("akhir dari program");
+
+     }
+}
